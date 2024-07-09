eb5c6ee1894c16021a742dc47fb35e15e252a0f4
Kazutaka0517 <iamkazutakaeyama@icloud.com>
2024-07-09 15:45:52 +0900

    カリキュラム08－2完了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+
 class PostController extends Controller
 {
-    public function test(Post $test)//インポートしたPostをインスタンス化して$postとして使用。
+    public function index (post $post)
     {
-        return $test->get();//$postの中身を戻り値にする。
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
 }
-}
+?>
