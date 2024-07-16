00a707c380831b58e67dd63bc33a8f740f34f4f8
Kazutaka0517 <iamkazutakaeyama@icloud.com>
2024-07-16 23:43:04 +0900

    ADD08-6delete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,31 +10,31 @@ class PostController extends Controller
 {
      public function index (Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     
     
     public function show (Post $post)
     {
-        return view('posts.show')->with(['post' => $post]);
+        return view('posts/show')->with(['post' => $post]);
     }
     
     
     public function create()
     {
-        return view('posts.create');
+        return view('posts/create');
     } 
     
     public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
-        return redirect('/posts.'.$post->id);
+        return redirect('/posts/'.$post->id);
     }
    
    public function edit(Post $post)
    {
-       return view('posts.edit')->with(['post'=>$post]);
+       return view('posts/edit')->with(['post'=>$post]);
    }
     
    public function update(PostRequest $request, Post $post)
@@ -42,6 +42,12 @@ class PostController extends Controller
        $input_post = $request['post'];
        $post->fill($input_post)->save();
        return redirect('/posts/' . $post->id);
+   }
+   
+   public function delete(Post $post)
+   {
+       $post->delete();
+       return redirect('/posts');
    }
 }
 ?>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,19 +10,29 @@
 
     <body>
         <h1>Blog name</h1>
-        <a href="/posts.create">create</a>
+        <a href="/posts/create">create</a>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-
-                    <a href="/posts/{{$post->id}}"><h2 class='title'>{{ $post->title }}</h2></a>
-
+                    <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
                     <p class='body'>{{$post->body}}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                       @csrf
+                       @method('DELETE')
+                       <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                    </form>
                 </div>
             @endforeach
         </div>
-        <div class='paginate'> 
-            {{$posts->links()}}
-        </div>
+        <div class='paginate'>{{$posts->links()}}</div>
+        <script>
+            function deletePost(id) {
+                'use strict'
+                
+                if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
     </body>
 </html>
