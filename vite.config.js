5a1614c1df721a38197cce6726850b48cbe432b4
jagaimo310 <sawashin2003310@gmail.com>
2024-08-23 00:43:12 +0900

    Google MAP APIの導入及びテスト

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/map.js',
+                'resources/css/map.css',
+
             ],
             refresh: true,
         }),
