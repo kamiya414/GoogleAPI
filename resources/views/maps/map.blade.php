b2eb352daffd69ae71f03eb19cfd40dfa6d68c7e
jagaimo310 <sawashin2003310@gmail.com>
2024-08-25 16:44:55 +0900

    投稿機能の実装

--- resources/views/maps/map.blade.php
+++ resources/views/maps/map.blade.php
@@ -2,19 +2,20 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-   <link rel="stylesheet" type="text/css" href="{{ asset('/build/assets/map-CN376zLE.css') }}"> 
+    <a href="/posts/create">投稿</a>
+    <link rel="stylesheet" type="text/css" href="{{ asset('/build/assets/map-CN376zLE.css') }}"> 
 </head>
 
 
-<body>
-    <input
-        id="pac-input"
-        class="controls"
-        type="text"
-        placeholder="Search Box"
-    />
-    <div id="map" style="width:800px; height:800px; margin: auto">
-        <script src="{{ asset('/build/assets/map.js') }}"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{ config("services.google-map.apikey") }}&callback=initAutocomplete&libraries=places&v=weekly"defer></script>
-    </div>
-</body>
+    <body>
+        <input
+            id="pac-input"
+            class="controls"
+            type="text"
+            placeholder="Search Box"
+        />
+        <div id="map" style="width:800px; height:800px; margin: auto">
+            <script src="{{ asset('/build/assets/map.js') }}"></script>
+            <script src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{ config("services.google-map.apikey") }}&callback=initAutocomplete&libraries=places&v=weekly"defer></script>
+        </div>
+    </body>
